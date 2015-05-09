fb468de4ecc2920b232696fc532d6189c8d28626
Laurent LAPORTE <tantale.solutions@gmail.com>
2015-05-09 16:12:28 +0200

    Setup the ``.gitignore`` and ``.bumpversion.cfg`` (set version to "0.1.0") and add a basic ``setup.py`` file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bumpversion_demo',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/tantale/bumpversion_demo',
+    license='MIT License',
+    author='Tantale',
+    author_email='tantale.solution@gmail.com',
+    description='Demonstration of ``bumpversion`` usage in the context of a Python project.'
+)
